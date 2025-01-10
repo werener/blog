979d42854ca80251f319929ea06e68c2357cec51
kroften <sskozhemyakin2006@gmail.com>
2025-01-10 17:37:35 +0300

    feat: Add swagger and temporary db

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,46 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const tempDB = {
+    gallery: [
+        {
+            img: "https://i.imgur.com/q6hF7OO.jpeg",
+            name: "slut",
+            description: "merry christmas"
+        },
+        {
+            img: "https://i.imgur.com/q6hF7OO.jpeg",
+            name: "slut2",
+            description: "feliz navidad"
+        },{
+            img: "https://i.imgur.com/q6hF7OO.jpeg",
+            name: "slut3",
+            description: "ho ho ho"
+        },
+    ],
+    projects: [
+        {
+            carousel: ["https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg"],
+            name: "many sluts",
+            link: "https://msun.itch.io/okayga",
+            description: "have yourself a merry little christmas"
+        },
+        {
+            carousel: ["https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg"],
+            name: "more sluts",
+            link: "https://msun.itch.io/okayga",
+            description: "kill yourself",
+        },
+    ]
+};
+
+const app = new Elysia()
+    .use(swagger())
+    .get("/", () => "Hello Elysia")
+    .get("/gallery", tempDB.gallery)
+    .get("/projects", tempDB.projects)
+    .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
