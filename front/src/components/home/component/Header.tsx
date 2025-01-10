05768b7832fee02dacb1d728c5b17f7bb2209486
муен <167233615+miaeun@users.noreply.github.com>
2025-01-10 21:18:52 +0300

    style: Added CSS to Header
    Дописала немного всего в header, наложила несколько стилей и добавила картинки

--- front/src/components/home/component/Header.tsx
+++ front/src/components/home/component/Header.tsx
@@ -1,5 +1,20 @@
 function Header() {
-	return <h3>Head</h3>;
+	return (
+		<div className="header">
+			<div className="logo">
+				<h2>
+				<img src="../CSS/blog_logo.png" />
+				blog</h2>
+			</div>
+			<div className="icons">
+				<img src="../CSS/discord.png" alt="discord" />
+				<img src="" alt="telegram" />
+				<img src="" alt="youtube" />
+			</div>
+		</div>
+	);
 }
-
+import "../CSS/discord.png";
+import "../CSS/blog_logo.png";
+import "../CSS/Header.css";
 export default Header;
