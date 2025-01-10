f94a29da8b210828fdb9e911db9cfb18b04659bb
werener <lenyazhuk@yandex.com>
2025-01-10 16:48:03 +0300

    fix: renamed import

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./components/index/App.tsx";
+import App from "./components/home/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
