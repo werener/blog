e20f7635c85305449f3f6211d41ea9f63fca8f41
kroften <sskozhemyakin2006@gmail.com>
2025-01-10 19:13:45 +0300

    refactor: Move api to its own plugin, change endpoints

---
+++ back/src/api.ts
@@ -0,0 +1,60 @@
+import { Elysia, t } from "elysia";
+
+const tempDB = {
+    gallery: [
+        {
+            img: "https://i.imgur.com/q6hF7OO.jpeg",
+            name: "slut",
+            description: "merry christmas"
+        },
+        {
+            img: "https://i.imgur.com/q6hF7OO.jpeg",
+            name: "slut2",
+            description: "feliz navidad"
+        },{
+            img: "https://i.imgur.com/q6hF7OO.jpeg",
+            name: "slut3",
+            description: "ho ho ho"
+        },
+    ],
+    projects: [
+        {
+            carousel: ["https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg"],
+            name: "many sluts",
+            link: "https://msun.itch.io/okayga",
+            description: "have yourself a merry little christmas"
+        },
+        {
+            carousel: ["https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg"],
+            name: "more sluts",
+            description: "kill yourself",
+        },
+    ]
+};
+
+export const api = new Elysia({ prefix: "/api" })
+    .get("/gallery",
+         tempDB.gallery,
+         {
+            response: t.Array(
+                t.Object({
+                    img: t.String(),
+                    name: t.String(),
+                    description: t.String()
+                })
+            )
+         }
+        )
+    .get("/projects",
+         tempDB.projects,
+         {
+            response: t.Array(
+                t.Object({
+                    carousel: t.Array(t.String()),
+                    name: t.String(),
+                    link: t.Optional(t.String()),
+                    description: t.String()
+                })
+            )
+         }
+        );
--- back/src/index.ts
+++ back/src/index.ts
@@ -1,66 +1,12 @@
 import { Elysia, t } from "elysia";
 import { swagger } from "@elysiajs/swagger";
+import { api } from "./api"
 
-const tempDB = {
-    gallery: [
-        {
-            img: "https://i.imgur.com/q6hF7OO.jpeg",
-            name: "slut",
-            description: "merry christmas"
-        },
-        {
-            img: "https://i.imgur.com/q6hF7OO.jpeg",
-            name: "slut2",
-            description: "feliz navidad"
-        },{
-            img: "https://i.imgur.com/q6hF7OO.jpeg",
-            name: "slut3",
-            description: "ho ho ho"
-        },
-    ],
-    projects: [
-        {
-            carousel: ["https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg"],
-            name: "many sluts",
-            link: "https://msun.itch.io/okayga",
-            description: "have yourself a merry little christmas"
-        },
-        {
-            carousel: ["https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg", "https://i.imgur.com/q6hF7OO.jpeg"],
-            name: "more sluts",
-            description: "kill yourself",
-        },
-    ]
-};
 
 const app = new Elysia()
     .use(swagger())
-    .get("/", () => "Hello Elysia")
-    .get("/gallery",
-         tempDB.gallery,
-         {
-            response: t.Array(
-                t.Object({
-                    img: t.String(),
-                    name: t.String(),
-                    description: t.String()
-                })
-            )
-         }
-        )
-    .get("/projects",
-         tempDB.projects,
-         {
-            response: t.Array(
-                t.Object({
-                    carousel: t.Array(t.String()),
-                    name: t.String(),
-                    link: t.Optional(t.String()),
-                    description: t.String()
-                })
-            )
-         }
-        )
+    .get("/", () => "Hello Elysia\nlocalhost:3000/swagger")
+    .use(api)
     .listen(3000);
 
 console.log(
