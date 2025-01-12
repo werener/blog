d18b5022dde7cf906f460eb2b8c343e4c26ab632
werener <lenyazhuk@yandex.com>
2025-01-12 18:03:43 +0300

    feat: placed icons correctly
    Placed button to the topmost right corner

--- front/src/components/home/component/Header.tsx
+++ front/src/components/home/component/Header.tsx
@@ -4,28 +4,41 @@ import telegram from "../CSS/telegram.png";
 import youtube from "../CSS/youtube.png";
 import "../CSS/Header.css";
 
-function Header() {
+export default function Header() {
 	return (
 		<div className="header">
 			<div className="logo">
 				<h2>
-					<img className="logo-photo" src={blog_logo} width="50px"/>
+					<img className="logo-photo" src={blog_logo} width="50px" />
 					blog
 				</h2>
 			</div>
 			<div className="icons">
 				<div className="discord">
-					<img src={discord} alt="discord" width="50px"/>
+					<img
+						src={discord}
+						alt="discord"
+						width="100px"
+						className="discord-img"
+					/>
 				</div>
 				<div className="telegram">
-					<img src={telegram} alt="telegram" width="50px" />
+					<img
+						src={telegram}
+						alt="telegram"
+						width="64px"
+						className="telegram-img"
+					/>
 				</div>
 				<div className="youtube">
-					<img src={youtube} alt="youtube" width="50px"/>
+					<img
+						src={youtube}
+						alt="youtube"
+						width="100px"
+						className="youtube-img"
+					/>
 				</div>
 			</div>
 		</div>
 	);
 }
-
-export default Header;
