d8559115dd60937e7ae0c2450a788e63ff346a82
kroften <sskozhemyakin2006@gmail.com>
2025-01-12 22:50:19 +0300

    fix: Remove useless auth routes

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,7 +1,5 @@
 import { Elysia, t } from "elysia";
 import { dbRoutes } from "./db";
-import { authRoutes } from "./auth";
  
 export const api = new Elysia({ prefix: "/api" })
-    .use(dbRoutes)
-    .use(authRoutes);
+    .use(dbRoutes);
--- back/src/auth.ts
+++
@@ -1,6 +0,0 @@
-import { Elysia, t } from "elysia";
- 
-export const authRoutes = new Elysia({ prefix: "/auth" })
-    .post("/sign-up", ({ error }) => error(501))
-    .post("/sign-in", ({ error }) => error(501))
-    .post("/log-out", ({ error }) => error(501))
