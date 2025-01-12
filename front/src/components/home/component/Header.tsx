fce766bd2fa3c0682d15c950f6b8b69a9180eea8
муен <167233615+miaeun@users.noreply.github.com>
2025-01-12 17:00:48 +0300

    style: Fixed the icons

--- front/src/components/home/component/Header.tsx
+++ front/src/components/home/component/Header.tsx
@@ -1,21 +1,28 @@
 import discord from "../CSS/discord.png";
 import blog_logo from "../CSS/blog_logo.png";
+import telegram from "../CSS/telegram.png";
+import youtube from "../CSS/youtube.png";
 import "../CSS/Header.css";
 
 function Header() {
-	console.log(typeof blog_logo);
 	return (
 		<div className="header">
 			<div className="logo">
 				<h2>
-					<img src={blog_logo} />
+					<img className="logo-photo" src={blog_logo} width="50px"/>
 					blog
 				</h2>
 			</div>
 			<div className="icons">
-				<img src={discord} alt="discord" />
-				<img src="" alt="telegram" />
-				<img src="" alt="youtube" />
+				<div className="discord">
+					<img src={discord} alt="discord" width="50px"/>
+				</div>
+				<div className="telegram">
+					<img src={telegram} alt="telegram" width="50px" />
+				</div>
+				<div className="youtube">
+					<img src={youtube} alt="youtube" width="50px"/>
+				</div>
 			</div>
 		</div>
 	);
