f8f87ddacd97ef51d84702542c07dc5c9fbb0a83
муен <167233615+miaeun@users.noreply.github.com>
2025-01-13 15:59:42 +0300

    feature: Added dataSlider.tsx (not ended)
    i need some help actually

--- front/src/components/home/component/BtnSlider.tsx
+++ front/src/components/home/component/BtnSlider.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "..CSS/Slider.css";
+import "../CSS/Slider.css";
 import leftArrow from "../CSS/leftArrow.png";
 import rightArrow from "../CSS/rightArrow.png";
-export default function BtnSlider() {
+export default function BtnSlider({ direction, moveSlide }) {
     return (
-        <button className="btn-slide">
-            <img src={rightArrow} />
+        <button onClick={moveSlide} className={direction === "next" ? "btn-slide next" : "btn-slide prev"}>
+            <img src={direction === "next" ? rightArrow : leftArrow} />
         </button>
     );
 }
--- front/src/components/home/component/Slider.tsx
+++ front/src/components/home/component/Slider.tsx
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
-import "../CSS/Header.css";
+import "../CSS/Slider.css";
 import BtnSlider from "./BtnSlider";
 import dataSlider from "./dataSlider";
 
 export default function Slider() {
     const [slideIndex, setSlideIndex] = useState(1);
     const nextSlide = () => {
+        if (slideIndex !== dataSlider.length) {
+            setSlideIndex(slideIndex + 1)
+        }
+        else if (slideIndex === dataSlider.lenth) {
+            setSlideIndex(1)
+        }
     }
     const prevSlide = () => {
-
+        if (slideIndex !== 1) {
+            setSlideIndex(slideIndex - 1)
+        }
+        else if (slideIndex === 1) {
+            setSlideIndex(dataSlider.length)
+        }
+    }
+    const moveDot = index => {
+        setSlideIndex(index)
     }
     return (
         <div className="container-slider">
             {dataSlider.map((obj, index) => {
                 return (
                     <div key={obj.id} className={slideIndex === index+1 ? "slide active-anim" : "slide"}>
-                        <img src={ProcessingInstruction.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} />
+                        <img src={'/home' + `/Imgs/img${index + 1}.jpg`} />
                     </div>
                 )
             })}
